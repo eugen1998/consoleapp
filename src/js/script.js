93ff08b26bb72130827eea25c3f1c2a4285db4c5
Evgeny Kudryashov <eugen030498@mail.ru>
2020-06-09 15:44:20 +0400

    part 3

--- src/js/script.js
+++ src/js/script.js
@@ -23,9 +23,17 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-console.log(numberOfFilms);
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -35,12 +43,72 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastmovie = prompt('Один из последних просмотренных фильмов?', ''),
-    grade = +prompt('На сколько оцените его?', ''),
-    lastmovieTwo = prompt('Один из последних просмотренных фильмов?', ''),
-    gradeTwo = +prompt('На сколько оцените его?', '');
+// const lastmovie = prompt('Один из последних просмотренных фильмов?', ''),        //из практики ч. 1
+//     grade = +prompt('На сколько оцените его?', ''),
+//     lastmovieTwo = prompt('Один из последних просмотренных фильмов?', ''),
+//     gradeTwo = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastmovie] = grade;
-personalMovieDB.movies[lastmovieTwo] = gradeTwo;
+// personalMovieDB.movies[lastmovie] = grade;
+// personalMovieDB.movies[lastmovieTwo] = gradeTwo;
 
-console.log(personalMovieDB);
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const lastmovie = prompt('Один из последних просмотренных фильмов?', ''),
+            grade = +prompt('На сколько оцените его?', '');
+
+        if (lastmovie != null && grade != null && lastmovie != '' && grade != '' && lastmovie.length < 50) {
+            personalMovieDB.movies[lastmovie] = grade;
+            console.log('Done');
+        } else {
+            console.log('Error');
+            i--;
+        }
+    }
+}
+
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+// detectPersonalLevel();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
